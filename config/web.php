16b5c4445ac2339562f8efdac03a666932c42eaf
Gidah Thomas <gidamasaudathomas@gmail.com>
2026-08-29 13:00:26 +0300

    Set app timezone to East Africa Time so dashboard greeting and timestamps are correct

--- config/web.php
+++ config/web.php
@@ -6,6 +6,7 @@ $db = require __DIR__ . '/db.php';
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
+    'timeZone' => 'Africa/Nairobi',
     'defaultRoute'=>'dashboard/admin-dash',
     'bootstrap' => ['log'],
     'aliases' => [
